1c6b66d9196255af76caff6bb645d1c6f606b87e
Nalankumar <thirunalankumar@gmail.com>
2025-01-08 14:25:48 +0530

    Initial project set up

---
+++ src/main/java/com/tnk/wordnest/WordnestApplication.java
@@ -0,0 +1,13 @@
+package com.tnk.wordnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordnestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tnk/wordnest/WordnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tnk.wordnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
